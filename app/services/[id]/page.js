fdb33cc287d776d24d79bd800cb619dac442a7aa
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-12 14:47:41 +0600

    fix 3.

--- app/services/[id]/page.js
+++ app/services/[id]/page.js
@@ -4,7 +4,9 @@ import { getData } from "@/src/utils/getData";
 
 async function getSingleNews(id) {
   const res = await fetch(
-    `https://tender-server.vercel.app/api/v1/services/service-details/${id}`
+    `https://tender-server.vercel.app/api/v1/services/service-details/${id}`, {
+       cache: 'no-store'
+       }
   );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -27,6 +29,7 @@ console.log(data?.data?.thumbnail)
       <title>Tender Trading Inc</title>
       {/* <meta name="description" content="Tender Trading Inc." /> */}
       <meta name="description" content={`${data?.data?.description.slice(0, 100)}`} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <meta property="og:title" content={` ${data?.data?.title}`} />
       <meta
         property="og:description"
